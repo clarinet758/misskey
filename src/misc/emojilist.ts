fd75f54b3b9486d6ea415e2a148e171a62f0fc5d
mei23 <m@m544.net>
2019-09-15 00:57:11 +0900

    wip emojilist

---
+++ src/misc/emojilist.ts
@@ -0,0 +1,6 @@
+export const emojilist = require('../emojilist.json') as {
+	name: string;
+	keywords: string[];
+	char: string;
+	category: 'people' | 'animals_and_nature' | 'food_and_drink' | 'activity' | 'travel_and_places' | 'objects' | 'symbols' | 'flags';
+}[];
