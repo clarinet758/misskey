90830ae379884de797a246abb817b22bb01e2b38
mei23 <m@m544.net>
2020-01-03 17:29:31 +0900

    detect-url-mine

--- src/misc/detect-url-mime.ts
+++ src/misc/detect-url-mime.ts
@@ -2,7 +2,7 @@ import { createTemp } from './create-temp';
 import { downloadUrl } from './donwload-url';
 import { detectType } from './get-file-info';
 
-export async function detectUrlMine(url: string) {
+export async function detectUrlMime(url: string) {
 	const [path, cleanup] = await createTemp();
 
 	try {
